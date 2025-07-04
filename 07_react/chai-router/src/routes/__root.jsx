46db780d28cdd8771bf11850e371c49b1ee1f924
codebydeep <deepanshvish123@gmail.com>
2025-07-04 20:00:13 +0530

    Adding Zustand in 07_react

--- 07_react/chai-router/src/routes/__root.jsx
+++ 07_react/chai-router/src/routes/__root.jsx
@@ -2,11 +2,15 @@ import * as React from 'react'
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { Link } from "@tanstack/react-router"
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools"
+import { useCartStore } from '../store/cartStore'
 export const Route = createRootRoute({
   component: RootComponent,
 })
 
 function RootComponent() {
+  const cartCount = useCartStore((state) => state.cart.length)
+  const addToCart = useCartStore((state) => state.addToCart)
+
   return (
     <React.Fragment>
       <div>Hello "__root"!</div>
