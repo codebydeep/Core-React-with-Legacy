ba181a2051f9ea82eb5f8c95defaae938ac4f328
codebydeep <deepanshvish123@gmail.com>
2025-07-04 19:27:49 +0530

    Added Notification Provider

--- 07_react/chai-router/src/main.jsx
+++ 07_react/chai-router/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { NotificationProvider } from './context/NotificationContext'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
@@ -14,7 +15,9 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <NotificationProvider>
+        <RouterProvider router={router} />
+      </NotificationProvider>
     </StrictMode>,
   )
 }
